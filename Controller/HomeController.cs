fc705ff6dbfe99c501341b9dc404b31db7b08bb9
Paulo <paulo__alexandre.souza@hotmail.com>
2023-01-10 22:00:39 -0300

    no message

--- Controller/HomeController.cs
+++ Controller/HomeController.cs
@@ -1,12 +1,29 @@
+using System.Linq;
+using System.Threading.Tasks;
+using EstoqueWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EstoqueWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly EstoqueWebContext _context;
+
+        public HomeController(EstoqueWebContext context)
         {
-            return View();
+            this._context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var pedidos = await _context.Pedidos
+                .Where(p => !p.DataPedido.HasValue)
+                .Include(p => p.Cliente)
+                .OrderByDescending(p => p.IdPedido)
+                .AsNoTracking().ToListAsync();
+
+            return View(pedidos);
         }
     }
 }
